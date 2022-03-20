53cd89d5875c940dca88c8b02565873f38e8a4c0
dimadani <dikia656@gmail.com>
2022-03-20 11:27:27 +0700

    feat(pages): searching book data using filtering map

--- src/pages/UserBook.js
+++ src/pages/UserBook.js
@@ -4,6 +4,7 @@ import { CardUser, Footer, Navbars } from '../components';
 
 const UserBook = () => {
   const [books, setBooks] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch('http://localhost:3000/books')
@@ -12,6 +13,20 @@ const UserBook = () => {
       .catch((err) => console.log(err))
   },[]);
   
+  let inputHandler = (e) => {
+    let lowerCase = e.target.value.toLowerCase();
+    setSearch(lowerCase);
+  };
+
+  const filteredData = books.filter((el) => {
+    if (search === '') {
+        return el;
+    }
+    else {
+        return el.title.toLowerCase().includes(search);
+    }
+  });
+
   return (
     <Container>
         <Navbars />
@@ -19,21 +34,23 @@ const UserBook = () => {
             <Col xs={8} md={8} lg={8} className="text-center">
              <h3 className='fw-bold my-5'>Yudic Book’s Store</h3>
              <form className='d-flex flex-row' action=''>
-                <Input bsSize="sm" className='me-3 bg-light border-0 px-3 py-2' placeholder='Search book...' />
+                <Input bsSize="sm" className='me-3 bg-light border-0 px-3 py-2' placeholder='Search book...' onChange={inputHandler} />
                 <Button className='border-0 px-4 py-2' type='submit' style={{backgroundColor: '#FF6565'}}>search</Button>
              </form>
             </Col>
         </Row>
         <Row xs={1} md={2} lg={3} className='mt-5 m-3 bg-light justify-content-center rounded pt-3 pb-4'>
-          { books.map((book) => (
-            <CardUser 
-            key={book.id}
-            title={book.title} 
-            desc={book.description} 
-            author={book.author} 
-            harga={book.harga} 
-            image={book.url} />
-          )) }
+          {
+            filteredData.map((book) => (
+              <CardUser 
+              key={book.id}
+              title={book.title} 
+              desc={book.description} 
+              author={book.author} 
+              harga={book.harga} 
+              image={book.url} />
+            )) 
+          }
         </Row>
         <Footer />
     </Container>
